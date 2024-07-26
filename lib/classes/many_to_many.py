f36b5a34df11e1db3d052463a2cced0c4d4d120c
Tejas Nampalli <TSNampalli@gmail.com>
2024-07-25 21:15:01 -0400

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,78 @@
 class Game:
     def __init__(self, title):
         self.title = title
+        self._results = []
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if type(value) == str and len(value) > 0:
+            self._title = value 
 
     def results(self):
-        pass
+        return self._results
 
     def players(self):
-        pass
+        return list({result.player for result in self._results})
 
     def average_score(self, player):
-        pass
+        scores = [result.score for result in self._results if result.player == player]
+        if scores:
+            return sum(scores) / len(scores)
+
 
 class Player:
     def __init__(self, username):
         self.username = username
+        self._results = []
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, value):
+        if type(value) == str and 2 <= len(value) <= 16:
+            self._username = value
 
     def results(self):
-        pass
+        return self._results
 
     def games_played(self):
-        pass
+        return list({result.game for result in self._results})
 
     def played_game(self, game):
-        pass
+        played = [result.game for result in self._results if result.game == game]
+        if played:
+            return True
+        else:
+            return False 
 
     def num_times_played(self, game):
-        pass
+            return len([result for result in self._results if result.game == game])
 
 class Result:
+
+    all = []
+
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
-        self.score = score
+        self.score = score 
+        game._results.append(self)
+        player._results.append(self)
+        Result.all.append(self)
+    
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if hasattr(self, "score"):
+            return "Unable to change title"
+        if isinstance(score, int) and 1 <= score <= 5000:
+            self._score = score
